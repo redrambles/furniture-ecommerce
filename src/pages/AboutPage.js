a93a52c67e5ee929982ff6d67c3d5a59a5ad78a9
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-07-09 14:56:20 -0400

    About Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,37 +1,54 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+	return (
+		<main>
+			<PageHero title='about' />
+			<Wrapper className='page section section-center'>
+				<img src={aboutImg} alt='Beautiful Desk' />
+				<article>
+					<div className='title'>
+						<h2>Our Story</h2>
+						<div className='underline'></div>
+					</div>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus esse temporibus ullam et facilis delectus aliquid
+						obcaecati iste! Quod ex autem rem sint. Sapiente veritatis necessitatibus iure quo nobis officia.
+					</p>
+				</article>
+			</Wrapper>
+		</main>
+	);
+};
 
 const Wrapper = styled.section`
-  display: grid;
-  gap: 4rem;
-  img {
-    width: 100%;
-    display: block;
-    border-radius: var(--radius);
-    height: 500px;
-    object-fit: cover;
-  }
-  p {
-    line-height: 2;
-    max-width: 45em;
-    margin: 0 auto;
-    margin-top: 2rem;
-    color: var(--clr-grey-5);
-  }
-  .title {
-    text-align: left;
-  }
-  .underline {
-    margin-left: 0;
-  }
-  @media (min-width: 992px) {
-    grid-template-columns: 1fr 1fr;
-  }
-`
-export default AboutPage
+	display: grid;
+	gap: 4rem;
+	img {
+		width: 100%;
+		display: block;
+		border-radius: var(--radius);
+		height: 500px;
+		object-fit: cover;
+	}
+	p {
+		line-height: 2;
+		max-width: 45em;
+		margin: 0 auto;
+		margin-top: 2rem;
+		color: var(--clr-grey-5);
+	}
+	.title {
+		text-align: left;
+	}
+	.underline {
+		margin-left: 0;
+	}
+	@media (min-width: 992px) {
+		grid-template-columns: 1fr 1fr;
+	}
+`;
+export default AboutPage;
