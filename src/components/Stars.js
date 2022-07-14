c5d4a0eadceb7d20e5f944c507cd877c0fa713d5
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-07-14 09:00:56 -0400

    Add stars and color features for single products

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from "react";
+import styled from "styled-components";
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+
+const Stars = ({ stars, reviews }) => {
+	return (
+		<Wrapper>
+			<div className='stars'>
+				{new Array(5).fill(0).map((_, index) => (
+					<span>{stars >= index + 1 ? <BsStarFill /> : stars >= index + 1 - 0.5 ? <BsStarHalf /> : <BsStar />}</span>
+				))}
+			</div>
+			<div className='reviews'>{reviews} customer reviews</div>
+		</Wrapper>
+	);
+};
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  span {
-    color: #ffb900;
-    font-size: 1rem;
-    margin-right: 0.25rem;
-  }
-  p {
-    margin-left: 0.5rem;
-    margin-bottom: 0;
-  }
-  margin-bottom: 0.5rem;
-`
-export default Stars
+	display: flex;
+	align-items: center;
+	span {
+		color: #ffb900;
+		font-size: 1rem;
+		margin-right: 0.25rem;
+	}
+	p {
+		margin-left: 0.5rem;
+		margin-bottom: 0;
+	}
+	margin-bottom: 0.5rem;
+`;
+export default Stars;
